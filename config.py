99a855d8bf8c07edb107760b6c4f3b6324f10597
oazecline-maker <oaze.cline@gmail.com>
2025-12-02 23:17:42 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# Seller username
+SELLER = "@maybetyr"
+
+# Admin Telegram ID
+ADMIN_ID = 123456789
+
+# Secret admin command
+ADMIN_SECRET_COMMAND = "admin_Xx92aLkq77Zp38"
